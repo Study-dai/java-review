240f56a19ef70fff3e95f9852540771657123d53
Study-dai <1215461373@qq.com>
2020-02-19 21:21:53 +0800

    1

---
+++ 方法使用/main.java
@@ -0,0 +1,16 @@
+package 方法使用;
+//实现一个方法实现两个整数相加
+public class main {
+    public static void main(String[] args) {
+        add(1,2);
+    }
+    public static int add(int x,int y){
+        int ret=0;
+        ret=x+y;
+        System.out.println(ret);
+        return ret;
+    }
+}
+/*
+3
+ */
