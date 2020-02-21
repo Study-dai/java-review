b6a305ce4894855961459fadd4f1233190fafd61
Study-dai <1215461373@qq.com>
2020-02-21 21:01:19 +0800

    没有返回值的方法

---
+++ 方法使用/main4.java
@@ -0,0 +1,15 @@
+package 方法使用;
+//没有返回值的方法
+public class main4 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        add(a,b);
+    }
+    public static void add(int a,int b){
+        System.out.println(a+b);
+    }
+}
+/*
+30
+ */
