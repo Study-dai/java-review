19a118ce12a997eccb36d974487ae290a895f81b
Study-dai <1215461373@qq.com>
2020-02-23 21:38:02 +0800

    数组定义

---
+++ 数组定义与使用/main.java
@@ -0,0 +1,18 @@
+package 数组定义与使用;
+/*
+什么是数组：
+数组本质上就是让我们能 "批量" 创建相同类型的变量
+创建方式： int [] arr=new int[3]{1,2,3};
+           int [] arr={1,2,3};
+ */
+public class main {
+    public static void main(String[] args) {
+        int [] arr={1,2,3};
+
+    }
+    //使用for each遍历数组
+    public static void BianL(int [] array){
+
+    }
+
+}
