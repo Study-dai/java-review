588be80d9f6cc18ae87c37d2aa866f2d253227ea
Study-dai <1215461373@qq.com>
2020-03-20 23:17:52 +0800

    包装类

---
+++ 包装类/main.java
@@ -0,0 +1,17 @@
+package 包装类;
+/*
+基本数据类型和包装类对应
+int-Integer byte-Byte short-Short long_Long float-Float
+double-Double char-Character boolean_Boolean
+ */
+public class main {
+    public static void main(String[] args) {
+        int i=1;
+        //自动装箱
+        Integer ii=i;
+        Integer ij=(Integer)i;
+        //自动拆箱
+        int j=ii;
+        int k=(int)ii;
+    }
+}
